6422519d3de66a8cb6d862ed5f6b511b51a59564
clement lloyd <clement@INCHN-CLEMENT.local>
2018-08-27 17:21:58 +0530

    Fixed compilation errors

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 import ScoreSelectorGrid from "./scoreSelectorGrid/RunSelector";
-import ExtranSelectorGrid from './Extras/ExtraGrid'
+import ExtranSelectorGrid from "./scoreSelectorGrid/ExtrasSelector";
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-  <div>
-    <ScoreSelectorGrid />
     <div>
-      <ExtranSelectorGrid />
+      <ScoreSelectorGrid />
+      <div>
+        <ExtranSelectorGrid />
+      </div>
     </div>
-</div>
   </Provider>
 );
 
--- src/scoreSelectorGrid/ExtrasSelector.js
+++ src/scoreSelectorGrid/ExtrasSelector.js
@@ -3,12 +3,15 @@ import ButtonGroup from "../common/ButtonGroup";
 import { connect } from "react-redux";
 import { SET_CURRENT_SCORE, SET_EXTRAS } from "../game/actions";
 
-
-
 class ExtranSelectorGrid extends React.Component {
   render() {
-    const input = ['W','N','B','Lb'];
-  return (<span>Extras <ButtonGroup input={input} handleClick={this.props.handleClick} /></span>);
+    const input = ["W", "N", "B", "Lb"];
+    return (
+      <span>
+        Extras{" "}
+        <ButtonGroup input={input} handleClick={this.props.handleClick} />
+      </span>
+    );
   }
 }
 
