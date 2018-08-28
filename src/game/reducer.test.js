8f0134f58c880bb74194b54b2d2d4bccabe59fb8
Rahul Bakale <rahuldb@thoughtworks.com>
2018-08-28 11:41:11 +0530

    Team score: add test for reducer

---
+++ src/game/reducer.test.js
@@ -0,0 +1,24 @@
+import reducer from './reducer'
+
+test('should return initial start when reducer is called for the first time', () => {
+    const expectedState = {
+        maxOvers: 20,
+        teams: {
+            "Team 1": {
+                runs: 120,
+                wickets: 7,
+                oversPlayed: 15.4,
+                batting: true
+
+            },
+            "Team 2": {
+                runs: 150,
+                wickets: 5,
+                oversPlayed: 20,
+                batting: false
+
+            }
+        }
+    }
+    expect(expectedState).toEqual(reducer(undefined, {type:'@@INIT'}));
+})
