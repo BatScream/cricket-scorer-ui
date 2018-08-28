bee5ff4d2e34efa7fbf1d1ee2a4b500155a47926
Rahul Bakale <rahuldb@thoughtworks.com>
2018-08-28 12:31:05 +0530

    Team score: team name can be different than the team key

--- src/game/reducer.test.js
+++ src/game/reducer.test.js
@@ -1,24 +1,26 @@
 import reducer from './reducer'
 
 test('should return initial start when reducer is called for the first time', () => {
+    
     const expectedState = {
         maxOvers: 20,
         teams: {
-            "Team 1": {
+            team1: {
+                name: "Team 1",
                 runs: 120,
                 wickets: 7,
                 oversPlayed: 15.4,
                 batting: true
-
             },
-            "Team 2": {
+            team2: {
+                name: "Team 2",
                 runs: 150,
                 wickets: 5,
                 oversPlayed: 20,
                 batting: false
-
             }
         }
     }
+
     expect(expectedState).toEqual(reducer(undefined, {type:'@@INIT'}));
 })
--- src/teamscore/teamscore.js
+++ src/teamscore/teamscore.js
@@ -10,19 +10,19 @@ const TeamScore = (props) => {
     const teamScore = []
 
     Object.keys(teams)
-        .map(teamName => getTeamScoreDisplayElement(teamName, teams[teamName], game.maxOvers))
+        .map(teamKey => getTeamScoreDisplayElement(teams[teamKey], game.maxOvers))
         .map((element, index) => (<div key={index} className="teamScore">{element}</div>))
         .forEach(element => teamScore.push(element))
 
     return teamScore
 }
 
-function getTeamScoreDisplayElement(teamName, team, maxOvers) {
+function getTeamScoreDisplayElement(team, maxOvers) {
 
     if (team.batting) {
-        return (<h3>{teamName}<span className="score">{team.runs}/{team.wickets} in {team.oversPlayed}/{maxOvers}</span></h3>)
+        return (<h3>{team.name}<span className="score">{team.runs}/{team.wickets} in {team.oversPlayed}/{maxOvers}</span></h3>)
     } else {
-        return (<p>{teamName} scored {team.runs}/{team.wickets} in {team.oversPlayed} overs</p>)
+        return (<p>{team.name} scored {team.runs}/{team.wickets} in {team.oversPlayed} overs</p>)
     }
 }
 
