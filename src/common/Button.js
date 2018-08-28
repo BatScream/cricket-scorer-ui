0e828d4aa8c62a5d96264262499a739323b63142
sreejith <kr.srjith@gmail.com>
2018-08-28 10:09:13 +0530

    Button styling for

--- src/common/Button.js
+++ src/common/Button.js
@@ -2,12 +2,34 @@ import React from "react";
 import "./Button.css";
 
 class Button extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      isSelected:false
+    }
+    this.getClassForButtonState = this.getClassForButtonState.bind(this)
+  }
+
+  getClassForButtonState(selected){
+    if(selected){
+     return 'selectedButtonClass' 
+    }else{
+     return 'buttonClass'
+
+    }
+  }
   render() {
     return (
       <button
-        className="buttonClass"
+        className={this.getClassForButtonState(this.state.isSelected)}
         id={this.props.element.label}
         onClick={() => {
+          this.setState(function (state, props) {
+            return {
+              isSelected: !state.isSelected
+            }
+           })
           this.props.handleClick(this.props.element.value);
         }}
       >
