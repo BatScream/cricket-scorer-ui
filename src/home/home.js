2bfb4daa5d9a0d90636f6df5c8ddcef542ec13cc
pallawik <pallawik@in-pallawik.local>
2018-08-30 18:53:58 +0530

    Added styles to the top bars

--- src/home/home.js
+++ src/home/home.js
@@ -3,16 +3,32 @@ import "./home.css";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import LiveScore from "./liveScore";
 import ScoreBoard from "./scoreBoard";
+import { Container, Row, Col } from "reactstrap";
 
 class Home extends React.Component {
   render() {
+
+    const divStyle = {
+      margin: "auto",
+      border: "5px solid black",
+      backgroundColor: "lightyellow",
+      width: "500px",
+      "text-align":"center"
+    };
+
     return (
       <HashRouter>
         <div>
-          <div className="segmented">
-            <NavLink to="/">Live Score</NavLink>
-            <NavLink to="/scoreboard">Score Board</NavLink>
-          </div>
+        <Container style={divStyle}>
+        <Row>
+          <Col>
+          <NavLink to="/"><button class="button">Live Score</button></NavLink>
+          </Col>
+          <Col>
+          <NavLink to="/scoreboard"><button class="button">Score Board</button></NavLink>
+          </Col>
+        </Row>
+          </Container>
           <div className="content">
             <Route exact path="/" component={LiveScore} />
             <Route path="/scoreboard" component={ScoreBoard} />
