eeff21b41e6b38e21ffbc80184a7e7b2cd9b68b6
BatScream <clement.lloyd@gmail.com>
2018-08-30 17:19:56 +0530

    added css for scoreboard

--- src/home/scoreBoard.js
+++ src/home/scoreBoard.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 import ConnectedTeamScore from "../teamscore/teamscore.js";
-import ConnectedBattingTableDisplay from "../BattingTable/battingTable"
+import ConnectedBattingTableDisplay from "../BattingTable/battingTable";
+import { Container, Row, Col } from "reactstrap";
 
 class ScoreBoard extends React.Component {
-    render() {
-        return (
-            <div>
-                <ConnectedTeamScore />
-                <ConnectedBattingTableDisplay />
-            </div>
-        )
-    }
+  render() {
+    const divStyle = {
+      margin: "auto",
+      border: "5px solid black",
+      backgroundColor: "lightyellow",
+      width: "500px"
+    };
+    return (
+      <Container style={divStyle}>
+        <Row>
+          <Col xs="3">
+            <ConnectedTeamScore />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ConnectedBattingTableDisplay />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default ScoreBoard;
