4475837b29fad28ab55da98f28fdd900e8f5f023
pallawik <pallawik@in-pallawik.local>
2018-08-30 17:38:21 +0530

    Bowler Stats printed upon second view

--- src/BattingTable/battingTable.js
+++ src/BattingTable/battingTable.js
@@ -24,6 +24,18 @@ const BattingTable = (props) => {
                 </tr>
                 {props.battingStatics}
         </table>
+        <h5>Bowling Table</h5>
+
+        <table className="table">
+                <tr className='tableHeadingRow'>
+                    <th className='tableHeading'>Bowler</th>
+                    <th className='tableHeading'>Overs</th>
+                    <th className='tableHeading'>Maiden</th>
+                    <th className='tableHeading'>Run</th>
+                    <th className='tableHeading'>Wickets</th>
+                </tr>
+                {props.bowlingStats}
+        </table>
   
            
         </div>
@@ -44,24 +56,33 @@ const getTableRowForPlayer = (playerName,battingStats) => {
 
 }
 
+const getBowlingRow = (playerName,bowlingStats) => {
+    return (
+        <tr className='tableRow'>
+            <td className='tableCol'>{playerName}</td>
+            <td className='tableCol'>{bowlingStats.overs}</td>
+            <td className='tableCol'>{bowlingStats.maiden}</td>
+            <td className='tableCol'>{bowlingStats.run}</td>
+            <td className='tableCol'>{bowlingStats.wicket}</td>
+        </tr>
+    )
+
+}
+
 
 const mapStateBattingTableToDisplay = state => {
     const game = state.game;
     const currentTeam = game.currentBatsmen.team;
     const playerDictionary = Object.assign({}, game.teams[currentTeam].players)
-
     const battingStaticsDict = game.battingStats
     const battingStatics = []
-
+    const bowlingStats = []
     const strike1 = game.currentBatsmen.players[0]
     const strike2 = game.currentBatsmen.players[1]
-
     const currentPlayerOnSrike1 = playerDictionary[strike1]
     const currentPlayerOnSrike2 = playerDictionary[strike2]
-
     battingStatics.push(getTableRowForPlayer(<b>{currentPlayerOnSrike1.name +'*'}</b>,battingStaticsDict[strike1]))
     battingStatics.push(getTableRowForPlayer(<b>{currentPlayerOnSrike2.name +'*'}</b>,battingStaticsDict[strike2]))
-
     Object.keys(battingStaticsDict).map(function (key, index) {
         const stats = battingStaticsDict[key];
         if (stats.isOut) {
@@ -74,10 +95,25 @@ const mapStateBattingTableToDisplay = state => {
 
     });
 
+    const currentBowlingTeam = game.currentBowler.team
+    const bowlingPlayerDict = game.teams[currentBowlingTeam].players
+
+    Object.keys(bowlingPlayerDict).map(function (key, index) {
+        const player = bowlingPlayerDict[key]
+        bowlingStats.push(getBowlingRow(player.name,player.bowlingStats))
+        // if (stats.isOut) {
+        //     const player = bowlingStaticsDict[key].
+        //     console.log(key+' '+player)
+
+        //     battingStatics.push(getTableRowForPlayer(player.name,stats))
+        // }
+        console.log("Current Player " + battingStatics);
+
+    });
      console.log(battingStatics)
 
 
-    return { "battingStatics": battingStatics };
+    return { "battingStatics": battingStatics,bowlingStats:bowlingStats };
 };
 
 
